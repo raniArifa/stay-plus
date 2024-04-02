1aa100eebfa37170d0b7e9dd928a83f920dbd4a1
Rao-Sabahat-Ali <raosabahat01@live.com>
2024-04-02 13:00:29 +0200

    feat: Add i18n

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/scss/theme.scss";
+import LanguageSwitcher from './LanguageSwitcher';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../i18n';
 
 const manrope = Manrope({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
@@ -15,15 +18,18 @@ const manrope = Manrope({
 
 export default function RootLayout({children}) {
   return (
+    <I18nextProvider i18n={i18n}>
     <html lang="en">
       <body
         suppressHydrationWarning={true}
         className={manrope.className}
       >
+        <LanguageSwitcher />
         <Navbar />
         <main>{children}</main>
         <Footer />
       </body>
     </html>
+    </I18nextProvider>
   );
 }
