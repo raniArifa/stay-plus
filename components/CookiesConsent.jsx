66fb9976f90c2dcf37b1c2b11e1b87903c8456f3
Arifa Rani <arifa.rani@outlook.com>
2024-05-17 11:16:08 +0200

    feat: added translations

--- components/CookiesConsent.jsx
+++ components/CookiesConsent.jsx
@@ -4,7 +4,10 @@ import { useState, useEffect } from "react";
 import { hasCookie, setCookie } from "cookies-next";
 import Link from "next/link";
 
+import { useTranslation } from "react-i18next";
+
 export default function CookiesConsent() {
+  const { t } = useTranslation();
   const [showConsent, setShowConsent] = useState(true);
   useEffect(() => {
     setShowConsent(hasCookie("localConsent"));
@@ -20,17 +23,21 @@ export default function CookiesConsent() {
   }
 
   return (
-    <div className="position-fixed bottom-0 z-7000 start-0 w-100 bg-dark text-light p-3">
+    <div
+      className="position-fixed bottom-0 start-0 w-100 bg-dark text-light p-2"
+      style={{ zIndex: 7000000 }}
+    >
+      {" "}
       <div className="container">
         <div className="d-flex justify-content-between align-items-center">
           <span className="fs-5">
-          We use cookies to improve your user experience, statistics and view customized offers. By browsing further you accept that necessary cookies are used. You can read more here at {" "}
+            {t ("cookies_consent")}
             <Link href={"/privacy-policy"} className="text-light">
-              Privacy Policy
+            {t ("privacy_policy")}
             </Link>
           </span>
           <button
-            className="btn btn-primary rounded-pill px-4"
+            className="btn btn-primary px-4 align-items-center"
             onClick={() => acceptCookie()}
           >
             Accept
